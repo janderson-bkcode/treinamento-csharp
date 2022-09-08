949ee88153a277735f07226efbef752de937206b
Willian Menezes <willian_menezes_santos@hotmail.com>
2022-09-08 14:01:29 -0300

    Resolvendo com classes

---
+++ 03-linguagem-programacao-C#/ExemplosLP/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace ExemplosLP
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public int Quantidade { get; set; }
+        public bool Status { get; set; }
+    }
+}
